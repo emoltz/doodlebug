cd999a8eb95d7416dd712b718fb32b1532912386
emoltz <es5541@nyu.edu>
2022-04-19 11:46:30 -0400

    cmake

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ethanshafranmoltz/Documents/NYU/Bridge 2021/Homework/HW13/doodlebug_finished
+# Install script for directory: /Users/ethanshafranmoltz/Documents/GitHub/doodlebug
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ethanshafranmoltz/Documents/NYU/Bridge 2021/Homework/HW13/doodlebug_finished/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ethanshafranmoltz/Documents/GitHub/doodlebug/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
